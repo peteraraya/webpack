9d5cf878376094d8c027e15713574a61b9bb485a
Peter <piteraraya@gmail.com>
2020-02-15 20:23:59 -0300

    Inicialización de mi proyecto webpack

---
+++ src/js/componentes.js
@@ -0,0 +1,13 @@
+import  "../css/componentes.css";
+
+
+// Agregar export
+export const saludar = (nombre) => {
+    console.log('Creando etiqueta h1');
+
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${nombre}`;
+
+    document.body.append(h1);
+}
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin()]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: false }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use:[
+                    {
+                        loader: 'file-loader',
+                        options:{
+                            esModule: false,
+                            name: 'assets/[name].[ext]' // copia la imagen y dejala en esta ruta con el mismo nombre y ext
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CleanWebpackPlugin(),
+    ]
+
+}
